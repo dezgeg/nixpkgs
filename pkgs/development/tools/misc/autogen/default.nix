be0567acab984b9e96fd64e5506afb35146c373a
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2015-08-24 01:29:08 +0300

    autogen: Split into multiple outputs
    In particular, this avoids runtime-depending on guile.

--- pkgs/development/tools/misc/autogen/default.nix
+++ pkgs/development/tools/misc/autogen/default.nix
@@ -9,6 +9,8 @@ stdenv.mkDerivation rec {
     sha256 = "1flnbnmkbqmbfgammkl8m36wrlk6rhpgnf9pdm6gdfhqalxvggbv";
   };
 
+  outputs = [ "dev" "bin" "lib" "out" "man" "info" ];
+
   nativeBuildInputs = [ which pkgconfig perl ];
   buildInputs = [ guile libxml2 ];
 
@@ -18,6 +20,17 @@ stdenv.mkDerivation rec {
     sed -i "s,sed '.*-I.*',sed 's/\\\(^\\\| \\\)-I/\\\1/g',g" configure
 
     substituteInPlace pkg/libopts/mklibsrc.sh --replace /tmp $TMPDIR
+  '';
+
+  postInstall = ''
+    mkdir -p $dev/bin
+    mv $bin/bin/autoopts-config $dev/bin
+
+    for f in $lib/lib/autogen/tpl-config.tlib $out/share/autogen/tpl-config.tlib; do
+      sed -e "s|$dev/include|/no-such-autogen-include-path|" -i $f
+      sed -e "s|$bin/bin|/no-such-autogen-bin-path|" -i $f
+      sed -e "s|$lib/lib|/no-such-autogen-lib-path|" -i $f
+    done
   '';
 
   #doCheck = true; # 2 tests fail because of missing /dev/tty
