316db54ec6ca23d659192b2bf8fdb07b0c4f0f3c
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2015-10-15 19:42:36 +0300

    libmng: Split into multiple outputs

--- pkgs/development/libraries/libmng/default.nix
+++ pkgs/development/libraries/libmng/default.nix
@@ -8,6 +8,8 @@ stdenv.mkDerivation rec {
     sha256 = "0l5wa3b9rr4zl49zbbjpapqyccqjwzkzw1ph3p4pk9p5h73h9317";
   };
 
+  outputs = [ "dev" "out" "man"];
+
   propagatedBuildInputs = [ zlib libpng libjpeg lcms2 ];
 
   meta = {
