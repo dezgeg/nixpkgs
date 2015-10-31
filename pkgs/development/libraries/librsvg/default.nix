0312952137c2e8e2d4844b0dbe32e02969815f9a
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2015-10-31 16:53:33 +0200

    librsvg: Split into multiple outputs

--- pkgs/development/libraries/librsvg/default.nix
+++ pkgs/development/libraries/librsvg/default.nix
@@ -13,6 +13,8 @@ stdenv.mkDerivation rec {
     sha256 = "0fplymmqqr28y24vcnb01szn62pfbqhk8p1ngns54x9m6mflr5hk";
   };
 
+  outputs = [ "dev" "out" "man" "docdev" ];
+
   NIX_LDFLAGS = if stdenv.isDarwin then "-lintl" else null;
 
   buildInputs = [ libxml2 libgsf bzip2 libcroco pango libintlOrEmpty ]
