d4054f5ea50aba72c92211e0c2579fc244b322d7
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2015-11-01 11:19:16 +0200

    gedit: Reference correct output of gtksourceview

--- pkgs/desktops/gnome-3/3.16/apps/gedit/default.nix
+++ pkgs/desktops/gnome-3/3.16/apps/gedit/default.nix
@@ -24,7 +24,7 @@ stdenv.mkDerivation rec {
     wrapProgram "$out/bin/gedit" \
       --set GDK_PIXBUF_MODULE_FILE "$GDK_PIXBUF_MODULE_FILE" \
       --prefix GI_TYPELIB_PATH : "$GI_TYPELIB_PATH" \
-      --prefix LD_LIBRARY_PATH : "${gnome3.libpeas}/lib:${gnome3.gtksourceview}/lib" \
+      --prefix LD_LIBRARY_PATH : "${gnome3.libpeas}/lib:${gnome3.gtksourceview.out}/lib" \
       --prefix XDG_DATA_DIRS : "${gnome3.gnome_themes_standard}/share:$XDG_ICON_DIRS:$GSETTINGS_SCHEMAS_PATH"
   '';
 
