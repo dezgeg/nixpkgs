0ac8af67746e14f7c98be0f53f96b029586c049c
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2015-11-03 00:24:05 +0200

    U-Boot: Refactor

--- pkgs/misc/uboot/default.nix
+++ pkgs/misc/uboot/default.nix
@@ -1,62 +1,77 @@
-{ stdenv, fetchurl, bc, dtc
-, toolsOnly ? false
-, defconfig ? "allnoconfig"
-, targetPlatforms
-, filesToInstall
-}:
+{ stdenv, fetchurl, bc, dtc }:
 
 let
-  platform = stdenv.platform;
-  crossPlatform = stdenv.cross.platform;
-  makeTarget = if toolsOnly then "tools NO_SDL=1" else "all";
-  installDir = if toolsOnly then "$out/bin" else "$out";
-  buildFun = kernelArch:
-    ''
-      if test -z "$crossConfig"; then
-          make ${makeTarget}
-      else
-          make ${makeTarget} ARCH=${kernelArch} CROSS_COMPILE=$crossConfig-
-      fi
-    '';
-in
+  mkUBoot = { defconfig, targetPlatforms, filesToInstall, installDir ? "", ... } @ args:
+    stdenv.mkDerivation (rec {
+      name = "uboot-${defconfig}-${version}";
+      version = "2015.10";
 
-stdenv.mkDerivation rec {
-  name = "uboot-${defconfig}-${version}";
-  version = "2015.10";
+      src = fetchurl {
+        url = "ftp://ftp.denx.de/pub/u-boot/u-boot-${version}.tar.bz2";
+        sha256 = "0m8r08izci0lzzjn5c5g5manp2rc7yc5swww0lxr7bamjigqvimx";
+      };
 
-  src = fetchurl {
-    url = "ftp://ftp.denx.de/pub/u-boot/u-boot-${version}.tar.bz2";
-    sha256 = "0m8r08izci0lzzjn5c5g5manp2rc7yc5swww0lxr7bamjigqvimx";
+      nativeBuildInputs = [ bc dtc ];
+
+      configurePhase = ''
+        make ${defconfig}
+      '';
+
+      installPhase = ''
+        mkdir -p $out/${installDir}
+        cp ${stdenv.lib.concatStringsSep " " filesToInstall} $out/${installDir}
+      '';
+
+      dontStrip = true;
+
+      crossAttrs = {
+        makeFlags = "ARCH=${stdenv.cross.platform.kernelArch} CROSS_COMPILE=${stdenv.cross.config}-";
+      };
+
+      meta = with stdenv.lib; {
+        homepage = "http://www.denx.de/wiki/U-Boot/";
+        description = "Boot loader for embedded systems";
+        license = licenses.gpl2;
+        maintainers = [ maintainers.dezgeg ];
+        platforms = targetPlatforms;
+      };
+    } // args);
+
+in {
+  ubootTools = mkUBoot {
+    defconfig = "allnoconfig";
+    targetPlatforms = stdenv.lib.platforms.linux;
+    filesToInstall = ["tools/dumpimage" "tools/mkenvimage" "tools/mkimage"];
+    installDir = "bin";
+
+    buildFlags = "tools NO_SDL=1";
+    dontStrip = false;
   };
 
-  patches = [ ./vexpress-Use-config_distro_bootcmd.patch ];
-
-  nativeBuildInputs = [ bc dtc ];
-
-  configurePhase = ''
-    make ${defconfig}
-  '';
-
-  buildPhase = assert (platform ? kernelArch);
-    buildFun platform.kernelArch;
-
-  installPhase = ''
-    mkdir -p ${installDir}
-    cp ${stdenv.lib.concatStringsSep " " filesToInstall} ${installDir}
-  '';
-
-  dontStrip = !toolsOnly;
-
-  crossAttrs = {
-    buildPhase = assert (crossPlatform ? kernelArch);
-      buildFun crossPlatform.kernelArch;
+  ubootJetsonTK1 = mkUBoot {
+    defconfig = "jetson-tk1_defconfig";
+    targetPlatforms = ["armv7l-linux"];
+    filesToInstall = ["u-boot" "u-boot.dtb" "u-boot-dtb-tegra.bin" "u-boot-nodtb-tegra.bin"];
   };
 
-  meta = with stdenv.lib; {
-    homepage = "http://www.denx.de/wiki/U-Boot/";
-    description = "Boot loader for embedded systems";
-    license = licenses.gpl2;
-    maintainers = [ maintainers.dezgeg ];
-    platforms = targetPlatforms;
+  ubootPcduino3Nano = mkUBoot {
+    defconfig = "Linksprite_pcDuino3_Nano_defconfig";
+    targetPlatforms = ["armv7l-linux"];
+    filesToInstall = ["u-boot-sunxi-with-spl.bin"];
+  };
+
+  ubootRaspberryPi = mkUBoot {
+    defconfig = "rpi_defconfig";
+    targetPlatforms = ["armv6l-linux"];
+    filesToInstall = ["u-boot.bin"];
+  };
+
+  # Intended only for QEMU's vexpress-a9 emulation target!
+  ubootVersatileExpressCA9 = mkUBoot {
+    defconfig = "vexpress_ca9x4_defconfig";
+    targetPlatforms = ["armv7l-linux"];
+    filesToInstall = ["u-boot"];
+
+    patches = [ ./vexpress-Use-config_distro_bootcmd.patch ];
   };
 }
