cb2ff55a9886a8d4b20bdc4c3bbd27aba309b4b5
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2016-01-10 16:45:25 +0200

    amoeba: init at 1.1
    This uses all the patches from Debian since otherwise it would work on
    i686 among other things.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13890,6 +13890,9 @@ let
 
   alienarena = callPackage ../games/alienarena { };
 
+  amoeba = callPackage ../games/amoeba { };
+  amoeba-data = callPackage ../games/amoeba/data.nix { };
+
   andyetitmoves = if stdenv.isLinux then callPackage ../games/andyetitmoves {} else null;
 
   anki = callPackage ../games/anki { };
