515c8ac2ef0a0555cc79a0e89e89d8f3bd7f19d9
Tuomas Tynkkynen <tuomas@tuxera.com>
2016-03-04 23:40:16 +0200

    libelf: Aarch64 hack

--- pkgs/development/libraries/libelf/default.nix
+++ pkgs/development/libraries/libelf/default.nix
@@ -8,8 +8,13 @@ stdenv.mkDerivation (rec {
     sha256 = "0vf7s9dwk2xkmhb79aigqm0x0yfbw1j0b9ksm51207qwr179n6jr";
   };
 
+  postPatch = if stdenv.isAarch64 then ''
+    cp ${../../../../config.guess} config.guess
+    cp ${../../../../config.sub} config.sub
+  '' else null;
+
   doCheck = true;
-  
+
   # For cross-compiling, native glibc is needed for the "gencat" program.
   crossAttrs = {
     nativeBuildInputs = [ glibc gettext ];
