56550bda7a6001265b8ef7037e4ebd076cbc6674
Tuomas Tynkkynen <tuomas@tuxera.com>
2016-03-05 21:46:10 +0200

    kexec-tools: Add Aarch64 patch
    It touches configure.ac so add autoreconfHook

--- pkgs/os-specific/linux/kexectools/default.nix
+++ pkgs/os-specific/linux/kexectools/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, zlib }:
+{ stdenv, fetchurl, autoreconfHook, zlib }:
 
 stdenv.mkDerivation rec {
   name = "kexec-tools-${version}";
@@ -12,7 +12,12 @@ stdenv.mkDerivation rec {
     sha256 = "1qrfka9xvy77k0rg3k0cf7xai0f9vpgsbs4l3bs8r4nvzy37j2di";
   };
 
-  buildInputs = [ zlib ];
+  buildInputs = [ autoreconfHook zlib ];
+
+  patches = stdenv.lib.optional stdenv.isAarch64 (fetchurl {
+    url = https://raw.githubusercontent.com/openembedded/openembedded-core/master/meta/recipes-kernel/kexec/kexec-tools/kexec-aarch64.patch;
+    sha256 = "0k2v6q21gf3yhrv5xwc63hvd23irqi6zq58qlxb66nq4i4giah95";
+  });
 
   meta = with stdenv.lib; {
     homepage = http://horms.net/projects/kexec/kexec-tools;
