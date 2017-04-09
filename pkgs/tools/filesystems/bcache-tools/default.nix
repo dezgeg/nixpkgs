71a612f56d91cf5027a2370ee9f919d058f9641b
Tuomas Tynkkynen <tuomas@tuxera.com>
2017-04-09 21:09:00 +0300

    bcache-tools: Switch to fetchFromGitHub

--- pkgs/tools/filesystems/bcache-tools/default.nix
+++ pkgs/tools/filesystems/bcache-tools/default.nix
@@ -1,12 +1,13 @@
-{ stdenv, fetchurl, pkgconfig, utillinux, bash }:
+{ stdenv, fetchFromGitHub, pkgconfig, utillinux, bash }:
 
 stdenv.mkDerivation rec {
   name = "bcache-tools-${version}";
   version = "1.0.7";
 
-  src = fetchurl {
-    name = "${name}.tar.gz";
-    url = "https://github.com/g2p/bcache-tools/archive/v${version}.tar.gz";
+  src = fetchFromGitHub {
+    owner = "g2p";
+    repo = "bcache-tools";
+    rev = "v${version}";
     sha256 = "1gbsh2qw0a7kgck6w0apydiy37nnz5xvdgipa0yqrfmghl86vmv4";
   };
 
@@ -43,7 +44,7 @@ stdenv.mkDerivation rec {
       Bcache is a Linux kernel block layer cache. It allows one or more fast
       disk drives such as flash-based solid state drives (SSDs) to act as a
       cache for one or more slower hard disk drives.
-      
+
       This package contains the required user-space tools.
 
       User documentation is in Documentation/bcache.txt in the Linux kernel
