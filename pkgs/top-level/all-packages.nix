3de63f2b07c4b700b520047a8720d76c6a17070b
Samuel Dionne-Riel <samuel@dionne-riel.com>
2017-12-10 20:54:11 -0500

    arm-trusted-firmware: Inits at various versions
    For qemu, at 1.4 from upstream.
    
    For Sopine / Pine A64-LTS, at 1.0 from fork.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12896,6 +12896,8 @@ with pkgs;
 
   inherit (callPackage ../misc/arm-trusted-firmware {})
     buildArmTrustedFirmware
+    armTrustedFirmwareAllwinner
+    armTrustedFirmwareQemu
     armTrustedFirmwareRK3328
     ;
 
