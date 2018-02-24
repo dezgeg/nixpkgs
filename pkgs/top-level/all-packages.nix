fb9343bf9a75eb68941c344fc55fe161e486f758
Ben Wolsieffer <benwolsieffer@gmail.com>
2018-02-23 20:29:24 -0500

    uboot: add Rock64 support
    The bootloader is currently non-functional because it lacks the ARM Trusted
    Firmware image (bl31.elf).

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13899,6 +13899,8 @@ with pkgs;
 
   ubootGuruplug = callPackage ../misc/uboot/guruplug.nix { };
 
+  ubootRock64 = callPackage ../misc/uboot/rock64.nix { };
+
   uclibc = callPackage ../os-specific/linux/uclibc { };
 
   uclibcCross = lowPrio (callPackage ../os-specific/linux/uclibc {
