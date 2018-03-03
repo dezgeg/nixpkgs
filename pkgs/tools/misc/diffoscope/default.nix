65798e58f0ed06a3b46a538848310b420292771b
Tuomas Tynkkynen <tuomas@tuxera.com>
2018-03-03 22:30:00 +0200

    diffoscope: Update tools list

--- pkgs/tools/misc/diffoscope/default.nix
+++ pkgs/tools/misc/diffoscope/default.nix
@@ -1,5 +1,5 @@
 { lib, stdenv, fetchgit, python3Packages, docutils, help2man
-, acl, apktool, libbfd, bzip2, cbfstool, cdrkit, colord, colordiff, coreutils, cpio, diffutils, dtc
+, acl, apktool, binutils-unwrapped, bzip2, cbfstool, cdrkit, colord, colordiff, coreutils, cpio, db, diffutils, dtc
 , e2fsprogs, file, findutils, fontforge-fonttools, fpc, gettext, ghc, ghostscriptX, giflib, gnupg1, gnutar
 , gzip, imagemagick, jdk, libarchive, libcaca, llvm, mono, openssh, pdftk, pgpdump, poppler_utils, sng, sqlite
 , squashfsTools, tcpdump, unoconv, unzip, xxd, xz
@@ -30,9 +30,9 @@ python3Packages.buildPythonApplication rec {
   # Still missing these tools: docx2txt enjarify js-beautify oggDump Rscript
   # Also these libraries: python3-guestfs
   pythonPath = with python3Packages; [ debian libarchive-c python_magic tlsh rpm ] ++ [
-      acl libbfd bzip2 cdrkit colordiff coreutils cpio diffutils dtc e2fsprogs file findutils
-      fontforge-fonttools gettext gnutar gzip libarchive libcaca pgpdump sng sqlite
-      squashfsTools unzip xxd xz
+      acl binutils-unwrapped bzip2 cdrkit colordiff coreutils cpio db diffutils
+      dtc e2fsprogs file findutils fontforge-fonttools gettext gnutar gzip
+      libarchive libcaca pgpdump sng sqlite squashfsTools unzip xxd xz
     ] ++ lib.optionals enableBloat [
       apktool cbfstool colord fpc ghc ghostscriptX giflib gnupg1 imagemagick
       llvm jdk mono openssh pdftk poppler_utils tcpdump unoconv
