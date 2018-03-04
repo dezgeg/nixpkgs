1e6081f3109f67a406c59142feef0f3443ecb71e
Ben Wolsieffer <benwolsieffer@gmail.com>
2018-03-03 21:18:05 -0500

    uboot: rock64: use binary blob for TPL
    This stops the kernel from crashing immediately after boot.

--- pkgs/misc/uboot/rock64.nix
+++ pkgs/misc/uboot/rock64.nix
@@ -1,4 +1,11 @@
-{ lib, buildUBoot, fetchFromGitHub, armTrustedFirmwareRK3328 }: buildUBoot {
+{ lib, buildUBoot, fetchFromGitHub, armTrustedFirmwareRK3328 }: let
+  rkbin = fetchFromGitHub {
+    owner = "ayufan-rock64";
+    repo = "rkbin";
+    rev = "d8b90685b3d93c358936babdd854f1018bc6d35e";
+    sha256 = "0wrh3xa968sdp0j9n692jnv3071ymab719zc56vllba0aaabiaxr";
+  };
+in buildUBoot {
   src = fetchFromGitHub {
     owner = "ayufan-rock64";
     repo = "linux-u-boot";
@@ -7,8 +14,10 @@
   };
 
   extraMakeFlags = [ "BL31=${armTrustedFirmwareRK3328}/bl31.elf" "u-boot.itb" "all" ];
+
+  # So close to being blob free... But U-Boot TPL causes the kernel to hang
   postBuild = ''
-    ./tools/mkimage -n rk3328 -T rksd -d tpl/u-boot-tpl.bin idbloader.img
+    ./tools/mkimage -n rk3328 -T rksd -d ${rkbin}/rk33/rk3328_ddr_786MHz_v1.06.bin idbloader.img
     cat spl/u-boot-spl.bin >> idbloader.img
     dd if=u-boot.itb of=idbloader.img seek=448 conv=notrunc
   '';
@@ -17,7 +26,9 @@
   targetPlatforms = ["aarch64-linux"];
   filesToInstall = [ "spl/u-boot-spl.bin" "tpl/u-boot-tpl.bin" "u-boot.itb" "idbloader.img"];
 
-  extraMeta = {
-    maintainers = [ lib.maintainers.lopsided98 ];
+  extraMeta = with lib; {
+    maintainers = [ maintainers.lopsided98 ];
+    # Because of the TPL blob
+    license = licenses.unfreeRedistributableFirmware;
   };
 }
