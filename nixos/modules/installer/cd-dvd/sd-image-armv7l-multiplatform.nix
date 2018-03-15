6da92ff9d82339a5e9ed393c7b76cb969677ed6c
Tuomas Tynkkynen <tuomas@tuxera.com>
2018-03-15 11:39:35 +0200

    sd-image: linux_rpi test hack

--- nixos/modules/installer/cd-dvd/sd-image-armv7l-multiplatform.nix
+++ nixos/modules/installer/cd-dvd/sd-image-armv7l-multiplatform.nix
@@ -25,7 +25,7 @@ in
   boot.loader.generic-extlinux-compatible.enable = true;
 
   boot.consoleLogLevel = lib.mkDefault 7;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_rpi;
   # The serial ports listed here are:
   # - ttyS0: for Tegra (Jetson TK1)
   # - ttymxc0: for i.MX6 (Wandboard)
